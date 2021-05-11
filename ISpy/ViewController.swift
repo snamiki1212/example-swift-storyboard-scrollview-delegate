06c40e7d5b12637547b146e2d7218872da8878f0
snamiki1212 <snamiki1212@gmail.com>
2021-05-11 09:13:49 -0700

    Update step1

--- ISpy/ViewController.swift
+++ ISpy/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var scrollview: UIScrollView!
+    @IBOutlet var imageview: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
