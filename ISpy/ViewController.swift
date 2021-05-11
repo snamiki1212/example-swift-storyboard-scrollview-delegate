a769019d3c7e08d0a482f290f43ddfac71a7b0b9
snamiki1212 <snamiki1212@gmail.com>
2021-05-11 09:31:19 -0700

    Update steps

--- ISpy/ViewController.swift
+++ ISpy/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet var scrollview: UIScrollView!
     @IBOutlet var imageview: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        scrollview.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        updateZoomFor(size: view.bounds.size)
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageview
+    }
+    
+    func updateZoomFor(size: CGSize){
+        let widthScale = size.width / imageview.bounds.width
+        let heightScale = size.height / imageview.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollview.minimumZoomScale = scale
+        scrollview.zoomScale = scale
     }
 
 
